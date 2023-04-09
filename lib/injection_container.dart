c007b73a3e9fb2c94a791b6e173850b515c4b618
Mohamzeem <Mohamzeem@gmail.com>
2023-04-09 16:52:54 +0200

    last

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,10 +1,10 @@
 import 'package:get_it/get_it.dart';
-import 'package:todo_app/blocs/switch_bloc/switch_bloc.dart';
+import 'blocs/app_bloc/app_bloc.dart';
 import 'blocs/task_bloc/task_bloc.dart';
 
 GetIt sl = GetIt.instance;
 
 Future<void> init() async {
   sl.registerFactory(() => TaskBloc());
-  sl.registerFactory(() => SwitchBloc());
+  sl.registerFactory(() => AppBloc());
 }
